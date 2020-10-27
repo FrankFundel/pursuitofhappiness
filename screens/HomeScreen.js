539bc84f584a816a50025a0c161d9cfea392b73a
Frank Fundel <frank.fundel@outlook.com>
2020-10-27 15:42:32 +0100

    added icon and splash

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { styles, Colors, Fonts } from '../styles';
+import CachedImage from '../components/CachedImage';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
@@ -8,8 +9,8 @@ export default class HomeScreen extends React.Component {
     return {
       title: 'Home',
       headerRight: () => (
-        <TouchableOpacity style={styles.headerButton} onPress={() => navigation.navigate("Account")} activeOpacity={0.8}>
-          
+        <TouchableOpacity style={styles.headerButton} onPress={() => params.uid && navigation.navigate("Account", {uid: params.uid})} activeOpacity={0.8}>
+          <CachedImage style={chatStyles.headerButtonImage} image={params.image} defaultSource={require("../assets/user.png")} />
         </TouchableOpacity>
       ),
     }
