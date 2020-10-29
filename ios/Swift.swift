4b329b5973087129e882eac825507adacdf36572
Frank Fundel <frankfundel@Franks-Mac.local>
2020-10-29 04:28:12 -0700

    some ios stuff

---
+++ ios/Swift.swift
@@ -0,0 +1,8 @@
+//
+//  Swift.swift
+//  pursuitofhappiness
+//
+//  Created by Frank Fundel on 28.10.20.
+//
+
+import Foundation
--- ios/pursuitofhappiness/AppDelegate.m
+++ ios/pursuitofhappiness/AppDelegate.m
@@ -54,6 +54,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
