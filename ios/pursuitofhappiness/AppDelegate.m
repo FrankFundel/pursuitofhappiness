62dc7788ddb36c4b0f6325c5a72f82e5ca406146
Frank Fundel <frankfundel@Franks-Mac.local>
2020-11-12 04:33:12 -0800

    splash and plsit

--- ios/pursuitofhappiness/AppDelegate.m
+++ ios/pursuitofhappiness/AppDelegate.m
@@ -54,7 +54,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [RNSplashScreen show];
+  //[RNSplashScreen show];
   return YES;
 }
 
